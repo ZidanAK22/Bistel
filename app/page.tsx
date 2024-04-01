08f016bcd761b62ae2d8d886f17ad925127b46f9
ZidanAK22 <zidanazrak@gmail.com>
2024-04-01 22:55:19 +0700

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main>
       <Link href="https://tokopedia.com" target="_blank"> Tokopedia</Link>
+      <p>TEST TEST TEST</p>
     </main>  
   );
 }
